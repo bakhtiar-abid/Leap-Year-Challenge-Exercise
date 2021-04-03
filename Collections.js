41725507eff295ce3e505783f3599029222873b0
bakhtiarabid <abid.bu16@gmail.com>
2021-04-03 16:02:19 +0600

    add new one

---
+++ Collections.js
@@ -0,0 +1,9 @@
+var yourName = prompt("Enter your name");
+
+var guestlist = ["Abid", "Jhon", "Lara", "Jason"];
+
+if (guestlist.includes(yourName)) {
+   alert("Welcome!");
+} else {
+   alert("Your name isn't match try again");
+}
